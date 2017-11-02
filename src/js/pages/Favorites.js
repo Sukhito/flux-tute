a48975fad35e3de6e7ffdf7997dd8c322e39f6f4
Sukhito Tanawal <sukhito@192.168.1.5>
2017-11-02 19:02:42 +0800

    init project

---
+++ src/js/pages/Favorites.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export default class Favorites extends React.Component {
+  render() {
+
+    return (
+      <div>
+        <h1>Archives</h1>
+      </div>
+    );
+  }
+}
